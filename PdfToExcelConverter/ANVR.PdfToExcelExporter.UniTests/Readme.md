3a64316db61c8cd02d9e0b0f13a27f527edc7f5b
Anish <anishmarokey@gmail.com>
2023-08-27 16:33:17 +0530

    provider unit test made 100%

---
+++ PdfToExcelConverter/ANVR.PdfToExcelExporter.UniTests/Readme.md
@@ -0,0 +1,4 @@
+To run unit test
+1. change path in generate_coverage_report.sh
+2. sudo chmod +x generate_coverage_report.sh give permission
+3.  ./generate_coverage_report.sh   
---
+++ PdfToExcelConverter/ANVR.PdfToExcelExporter.UniTests/generate_coverage_report.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+# Navigate to the root directory of your project
+cd /Users/anishvarghese/Desktop/Dotnet/netcore-Poc-on-mac/PdfToExcelConverter/ANVR.PdfToExcelExporter.UniTests
+
+# Directory path to the old coverage report
+oldCoverageReportPath="TestResults"
+
+# Delete the old coverage report directory if it exists
+if [ -d "$oldCoverageReportPath" ]; then
+    rm -r "$oldCoverageReportPath"
+    echo "Old coverage report directory deleted."
+fi
+
+# Run dotnet test with code coverage collection
+# dotnet test --collect:"XPlat Code Coverage"
+# echo "Attachments $Attachments"
+
+
+# Run the test command and capture the output
+output=$(dotnet test --collect:"XPlat Code Coverage" 2>&1)
+
+# Extract the coverage report file path using grep and sed
+coverage_path=$(echo "$output" | grep -o -m 1 '/[^[:space:]]*coverage\.cobertura\.xml' | sed -E 's|^[[:space:]]*||')
+
+# Print the coverage report file path
+if [ -n "$coverage_path" ]; then
+  echo "Coverage report file path: $coverage_path"
+else
+  # Print the message in red color and exit
+  echo -e "\e[31mCoverage report file path not found. Exiting...\e[0m"
+  exit 1  # Exit the script with an error code
+fi
+
+# Target directory for the coverage report
+targetDir="coveragereport"
+
+# Generate HTML report using reportgenerator
+~/.dotnet/tools/reportgenerator -reports:"$coverage_path" -targetdir:"$targetDir" -reporttypes:Html
+
+echo "Code coverage report generated at $targetDir"
+
+# Open the HTML report in the default web browser
+open "$targetDir/index.html"
