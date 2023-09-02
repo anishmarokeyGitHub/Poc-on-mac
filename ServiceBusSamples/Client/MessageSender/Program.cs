84275294ecf863350cfb513a9902c6b97d9e3746
Anish <anishmarokey@gmail.com>
2023-09-02 12:45:48 +0530

    receive logic also implemented

--- ServiceBusSamples/Client/MessageSender/Program.cs
+++ ServiceBusSamples/Client/MessageSender/Program.cs
@@ -5,11 +5,15 @@ using MessageSender.QueueProvider.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
-services.AddQueuesServices(options => 
-        { 
-            options.QueueName = ""; 
-            options.ConnectionString = ""; 
+services.AddQueuesServices(options =>
+        {
+            options.QueueName = "";
+            options.ConnectionString = "";
         });
 var serviceBuilder = services.BuildServiceProvider();
 var queueProvider = serviceBuilder.GetRequiredService<IQueueProvider>();
 await queueProvider.PushToQueueAsync<string>("Hello Queue");
+
+var messageFromQueue = await queueProvider.ReceiveFromQueueAsync<string>();
+
+Console.WriteLine("Received Message");
